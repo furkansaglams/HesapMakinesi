d086947371007027e2924d3b503e3b362dd6a704
furkansaglams <furkansaglam038@gmail.com>
2025-04-27 17:36:34 +0300

    calculator screen design completed

--- SimpleCalculator/Form1.Designer.cs
+++ SimpleCalculator/Form1.Designer.cs
@@ -28,13 +28,266 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.resualtScreenLabel = new System.Windows.Forms.Label();
+            this.numberOneButton = new System.Windows.Forms.Button();
+            this.numberTwoButton = new System.Windows.Forms.Button();
+            this.numberTreeButton = new System.Windows.Forms.Button();
+            this.numberSixButton = new System.Windows.Forms.Button();
+            this.numberFiveButton = new System.Windows.Forms.Button();
+            this.numberFourButton = new System.Windows.Forms.Button();
+            this.numberNineButton = new System.Windows.Forms.Button();
+            this.numberEightButton = new System.Windows.Forms.Button();
+            this.numberSevenButton = new System.Windows.Forms.Button();
+            this.equalsButton = new System.Windows.Forms.Button();
+            this.numberZeroButton = new System.Windows.Forms.Button();
+            this.SS = new System.Windows.Forms.Button();
+            this.dividingButton = new System.Windows.Forms.Button();
+            this.multiplicationButton = new System.Windows.Forms.Button();
+            this.minusButton = new System.Windows.Forms.Button();
+            this.plusButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // resualtScreenLabel
+            // 
+            this.resualtScreenLabel.BackColor = System.Drawing.Color.White;
+            this.resualtScreenLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resualtScreenLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.resualtScreenLabel.Location = new System.Drawing.Point(12, 9);
+            this.resualtScreenLabel.Name = "resualtScreenLabel";
+            this.resualtScreenLabel.Size = new System.Drawing.Size(318, 73);
+            this.resualtScreenLabel.TabIndex = 0;
+            this.resualtScreenLabel.Text = "0";
+            this.resualtScreenLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // numberOneButton
+            // 
+            this.numberOneButton.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.numberOneButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.numberOneButton.Location = new System.Drawing.Point(12, 97);
+            this.numberOneButton.Name = "numberOneButton";
+            this.numberOneButton.Size = new System.Drawing.Size(75, 75);
+            this.numberOneButton.TabIndex = 1;
+            this.numberOneButton.Text = "1";
+            this.numberOneButton.UseVisualStyleBackColor = false;
+            // 
+            // numberTwoButton
+            // 
+            this.numberTwoButton.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.numberTwoButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.numberTwoButton.Location = new System.Drawing.Point(93, 97);
+            this.numberTwoButton.Name = "numberTwoButton";
+            this.numberTwoButton.Size = new System.Drawing.Size(75, 75);
+            this.numberTwoButton.TabIndex = 2;
+            this.numberTwoButton.Text = "2";
+            this.numberTwoButton.UseVisualStyleBackColor = false;
+            // 
+            // numberTreeButton
+            // 
+            this.numberTreeButton.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.numberTreeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.numberTreeButton.Location = new System.Drawing.Point(174, 97);
+            this.numberTreeButton.Name = "numberTreeButton";
+            this.numberTreeButton.Size = new System.Drawing.Size(75, 75);
+            this.numberTreeButton.TabIndex = 3;
+            this.numberTreeButton.Text = "3";
+            this.numberTreeButton.UseVisualStyleBackColor = false;
+            // 
+            // numberSixButton
+            // 
+            this.numberSixButton.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.numberSixButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.numberSixButton.Location = new System.Drawing.Point(174, 178);
+            this.numberSixButton.Name = "numberSixButton";
+            this.numberSixButton.Size = new System.Drawing.Size(75, 75);
+            this.numberSixButton.TabIndex = 6;
+            this.numberSixButton.Text = "6";
+            this.numberSixButton.UseVisualStyleBackColor = false;
+            // 
+            // numberFiveButton
+            // 
+            this.numberFiveButton.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.numberFiveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.numberFiveButton.Location = new System.Drawing.Point(93, 178);
+            this.numberFiveButton.Name = "numberFiveButton";
+            this.numberFiveButton.Size = new System.Drawing.Size(75, 75);
+            this.numberFiveButton.TabIndex = 5;
+            this.numberFiveButton.Text = "5";
+            this.numberFiveButton.UseVisualStyleBackColor = false;
+            // 
+            // numberFourButton
+            // 
+            this.numberFourButton.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.numberFourButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.numberFourButton.Location = new System.Drawing.Point(12, 178);
+            this.numberFourButton.Name = "numberFourButton";
+            this.numberFourButton.Size = new System.Drawing.Size(75, 75);
+            this.numberFourButton.TabIndex = 4;
+            this.numberFourButton.Text = "4";
+            this.numberFourButton.UseVisualStyleBackColor = false;
+            // 
+            // numberNineButton
+            // 
+            this.numberNineButton.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.numberNineButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.numberNineButton.Location = new System.Drawing.Point(174, 259);
+            this.numberNineButton.Name = "numberNineButton";
+            this.numberNineButton.Size = new System.Drawing.Size(75, 75);
+            this.numberNineButton.TabIndex = 9;
+            this.numberNineButton.Text = "9";
+            this.numberNineButton.UseVisualStyleBackColor = false;
+            // 
+            // numberEightButton
+            // 
+            this.numberEightButton.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.numberEightButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.numberEightButton.Location = new System.Drawing.Point(93, 259);
+            this.numberEightButton.Name = "numberEightButton";
+            this.numberEightButton.Size = new System.Drawing.Size(75, 75);
+            this.numberEightButton.TabIndex = 8;
+            this.numberEightButton.Text = "8";
+            this.numberEightButton.UseVisualStyleBackColor = false;
+            // 
+            // numberSevenButton
+            // 
+            this.numberSevenButton.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.numberSevenButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.numberSevenButton.Location = new System.Drawing.Point(12, 259);
+            this.numberSevenButton.Name = "numberSevenButton";
+            this.numberSevenButton.Size = new System.Drawing.Size(75, 75);
+            this.numberSevenButton.TabIndex = 7;
+            this.numberSevenButton.Text = "7";
+            this.numberSevenButton.UseVisualStyleBackColor = false;
+            // 
+            // equalsButton
+            // 
+            this.equalsButton.BackColor = System.Drawing.Color.Bisque;
+            this.equalsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.equalsButton.Location = new System.Drawing.Point(174, 340);
+            this.equalsButton.Name = "equalsButton";
+            this.equalsButton.Size = new System.Drawing.Size(75, 75);
+            this.equalsButton.TabIndex = 12;
+            this.equalsButton.Text = "=";
+            this.equalsButton.UseVisualStyleBackColor = false;
+            // 
+            // numberZeroButton
+            // 
+            this.numberZeroButton.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.numberZeroButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.numberZeroButton.Location = new System.Drawing.Point(93, 340);
+            this.numberZeroButton.Name = "numberZeroButton";
+            this.numberZeroButton.Size = new System.Drawing.Size(75, 75);
+            this.numberZeroButton.TabIndex = 11;
+            this.numberZeroButton.Text = "0";
+            this.numberZeroButton.UseVisualStyleBackColor = false;
+            // 
+            // SS
+            // 
+            this.SS.BackColor = System.Drawing.Color.Red;
+            this.SS.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.SS.ForeColor = System.Drawing.Color.White;
+            this.SS.Location = new System.Drawing.Point(12, 340);
+            this.SS.Name = "SS";
+            this.SS.Size = new System.Drawing.Size(75, 75);
+            this.SS.TabIndex = 10;
+            this.SS.Text = "C";
+            this.SS.UseVisualStyleBackColor = false;
+            // 
+            // dividingButton
+            // 
+            this.dividingButton.BackColor = System.Drawing.Color.Pink;
+            this.dividingButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dividingButton.Location = new System.Drawing.Point(255, 340);
+            this.dividingButton.Name = "dividingButton";
+            this.dividingButton.Size = new System.Drawing.Size(75, 75);
+            this.dividingButton.TabIndex = 16;
+            this.dividingButton.Text = "/";
+            this.dividingButton.UseVisualStyleBackColor = false;
+            // 
+            // multiplicationButton
+            // 
+            this.multiplicationButton.BackColor = System.Drawing.Color.Pink;
+            this.multiplicationButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.multiplicationButton.Location = new System.Drawing.Point(255, 259);
+            this.multiplicationButton.Name = "multiplicationButton";
+            this.multiplicationButton.Size = new System.Drawing.Size(75, 75);
+            this.multiplicationButton.TabIndex = 15;
+            this.multiplicationButton.Text = "X";
+            this.multiplicationButton.UseVisualStyleBackColor = false;
+            // 
+            // minusButton
+            // 
+            this.minusButton.BackColor = System.Drawing.Color.Pink;
+            this.minusButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.minusButton.Location = new System.Drawing.Point(255, 178);
+            this.minusButton.Name = "minusButton";
+            this.minusButton.Size = new System.Drawing.Size(75, 75);
+            this.minusButton.TabIndex = 14;
+            this.minusButton.Text = "-";
+            this.minusButton.UseVisualStyleBackColor = false;
+            // 
+            // plusButton
+            // 
+            this.plusButton.BackColor = System.Drawing.Color.Pink;
+            this.plusButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.plusButton.Location = new System.Drawing.Point(255, 97);
+            this.plusButton.Name = "plusButton";
+            this.plusButton.Size = new System.Drawing.Size(75, 75);
+            this.plusButton.TabIndex = 13;
+            this.plusButton.Text = "+";
+            this.plusButton.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(336, 431);
+            this.Controls.Add(this.dividingButton);
+            this.Controls.Add(this.multiplicationButton);
+            this.Controls.Add(this.minusButton);
+            this.Controls.Add(this.plusButton);
+            this.Controls.Add(this.equalsButton);
+            this.Controls.Add(this.numberZeroButton);
+            this.Controls.Add(this.SS);
+            this.Controls.Add(this.numberNineButton);
+            this.Controls.Add(this.numberEightButton);
+            this.Controls.Add(this.numberSevenButton);
+            this.Controls.Add(this.numberSixButton);
+            this.Controls.Add(this.numberFiveButton);
+            this.Controls.Add(this.numberFourButton);
+            this.Controls.Add(this.numberTreeButton);
+            this.Controls.Add(this.numberTwoButton);
+            this.Controls.Add(this.numberOneButton);
+            this.Controls.Add(this.resualtScreenLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label resualtScreenLabel;
+        private System.Windows.Forms.Button numberOneButton;
+        private System.Windows.Forms.Button numberTwoButton;
+        private System.Windows.Forms.Button numberTreeButton;
+        private System.Windows.Forms.Button numberSixButton;
+        private System.Windows.Forms.Button numberFiveButton;
+        private System.Windows.Forms.Button numberFourButton;
+        private System.Windows.Forms.Button numberNineButton;
+        private System.Windows.Forms.Button numberEightButton;
+        private System.Windows.Forms.Button numberSevenButton;
+        private System.Windows.Forms.Button equalsButton;
+        private System.Windows.Forms.Button numberZeroButton;
+        private System.Windows.Forms.Button SS;
+        private System.Windows.Forms.Button dividingButton;
+        private System.Windows.Forms.Button multiplicationButton;
+        private System.Windows.Forms.Button minusButton;
+        private System.Windows.Forms.Button plusButton;
     }
 }
 
